184e68aa7e6eab202d0feb1e791a5d2ca47896b3
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-12 23:37:17 +0530

    Add: Program for linked list demo

---
+++ PracticeLinkedList/LinkedListDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinkedList
+{
+    public class LinkedListDemo
+    {
+        public void MethodLinkedList()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            //Addlast
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+            linkedList.AddLast(5);
+            foreach (int o in linkedList)
+            {
+                Console.WriteLine($"{o}");
+            }
+            //AddFirst
+            linkedList.AddFirst(6);
+            linkedList.AddFirst(7);
+            linkedList.AddFirst(8);
+            //Remove
+            linkedList.Remove(linkedList.First);
+            linkedList.Remove(7);
+            linkedList.RemoveFirst();
+            linkedList.RemoveLast();
+
+            foreach (int i in linkedList)
+            {
+                Console.WriteLine($"{i}");
+            }
+            Console.WriteLine("Count  " + linkedList.Count);
+        }
+    }
+}
---
+++ PracticeLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListDemo linkedListDemo = new LinkedListDemo();
+            linkedListDemo.MethodLinkedList();
+            Console.ReadLine();
+        }
+    }
+}
